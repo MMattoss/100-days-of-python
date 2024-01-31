2776c679db35a2a789966434df33b0208ccd5d23
Mateus <mattoss.barreto@gmail.com>
2024-01-31 13:16:02 -0300

    quiz project

---
+++ day-17-starter/question_data.py
@@ -0,0 +1,45 @@
+question_data = [
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "History",
+        "question":
+            "In World War II, Hawker Typhoons served in the Pacific theater.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"],
+    },
+    {
+        type: "boolean",
+        difficulty: "medium",
+        category: "Science &amp; Nature",
+        question: "Sound can travel through a vacuum.",
+        correct_answer: "False",
+        incorrect_answers: ["True"],
+    },
+    {
+        type: "boolean",
+        difficulty: "medium",
+        category: "Entertainment: Video Games",
+        question:
+            "In &quot;Starbound&quot;, the track played by the Decorated Music Box is named &quot;Atlas&quot;.",
+        correct_answer: "True",
+        incorrect_answers: ["False"],
+    },
+    {
+        type: "boolean",
+        difficulty: "medium",
+        category: "Entertainment: Video Games",
+        question:
+            "DragonForce&#039;s &#039;Through the Fire and Flames&#039; is widely considered to be the hardest song in the Guitar Hero series.",
+        correct_answer: "True",
+        incorrect_answers: ["False"],
+    },
+    {
+        type: "boolean",
+        difficulty: "medium",
+        category: "Science &amp; Nature",
+        question: "A person can get sunburned on a cloudy day.",
+        correct_answer: "True",
+        incorrect_answers: ["False"],
+    },
+]
